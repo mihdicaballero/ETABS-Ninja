ff54731e83e77f7911424f8ea82fba5c4dbef3b0
Mihdí Caballero <mihdicaballero@gmail.com>
2024-06-06 16:33:43 -0300

    Agrego funciones y notebook para model review inicial

--- etabsninja/notebooks/example.py
+++ etabsninja/notebooks/example.py
@@ -2,12 +2,13 @@ import sys
 import os
 from icecream import ic
 # Get the current working directory
+print(2+2)
 current_folder = os.getcwd()
-# Append the current folder to the system path (for .py files)
-sys.path.append(current_folder)
-
-from etabsninja.interface import test_etabs_connection
-# # Run this one time and that's it
-# SapModel,EtabsObject = connect_to_etabs()  sdfsd
-
+# Go up two directories
+parent_folder = os.path.dirname(os.path.dirname(current_folder))
+# Append the parent folder to the system path (for .ipynb files)
+sys.path.append(parent_folder)
+from etabsninja.interface import connect_to_etabs, test_etabs_connection
+# Run this one time and that's it
+SapModel,EtabsObject = connect_to_etabs()
 test_etabs_connection()
